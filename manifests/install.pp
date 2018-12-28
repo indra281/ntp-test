76af0dde5c67ac4c733dcec67b8d47b9b2cb137b
indrajeet <indra484ku@gmail.com>
2018-12-28 18:39:41 +0530

    sfdsf

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,6 @@
 class ntp::install inherits ntp {
   
-  #$restrict_ipv4 = '0.0.0.1'
-  #$restrict_ipv6 = '::1'
+ 
   package { $ntp::package:
   ensure => latest,
   notify => Service[$::ntp::ntp_service],
